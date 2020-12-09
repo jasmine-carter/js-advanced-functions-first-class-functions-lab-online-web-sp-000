7334185823171f66195a37d7f145a0b375dd19a5
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-09 23:55:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 function returnFirstTwoDrivers() {
   const firstTwo = function(array){
-    return array.slice(Math.max(arr.length - 2, 1))
+    return array.slice(Math.max(array.length - 2, 2))
   }
 }
