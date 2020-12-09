613f5abd6b0cb19300a3d3647e71007800bc7a4d
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-09 23:58:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers() {
+  const firstTwo = function(array){
+    return array.slice(0,2)
+  }
+}
+
 function returnLastTwoDrivers() {
   const lastTwo = function(array){
     return array.slice(Math.max(array.length - 2, 2))
