efe5df632eeff00f1c37050ac36526fdb2b5f36c
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-10 00:07:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ const returnLastTwoDrivers = function(drivers) {
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
 
 
-createFareMultiplier() = function(fare, integer){
+function createFareMultiplier() = function(fare, integer){
   return fare * integer
 }
